3fdd59feac474c5f5d009fb6f691be7e51c07c9a
avery nortonsmith <averynortonsmith@fastmail.com>
2025-11-04 22:28:05 -0500

    editor features

--- static/editor/editor.js
+++ static/editor/editor.js
@@ -10,21 +10,37 @@ function highlight() {
   editor.innerHTML = ptls.highlight(tokens);
 }
 
-const jar = CodeJar(editor, highlight);
+editor.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.key === "Enter") {
+    event.preventDefault();
+    run.onclick();
+  }
+});
+
+const jar = CodeJar(editor, highlight, {
+  tab: "  ",
+  moveToNewline: null,
+  addClosing: false,
+  autoclose: null,
+});
+
+setTimeout(() => editor.focus(), 50);
 
 function saveSource() {
   localStorage.setItem("source", jar.toString());
 }
 
-const sourceUrl = new URLSearchParams(location.search).get("source");
+function sourceUrl() {
+  return new URLSearchParams(location.search).get("source");
+}
 
 async function loadUrl(url) {
   const response = await fetch(url);
   return await response.text();
 }
 
-const source = sourceUrl
-  ? await loadUrl(sourceUrl)
+const source = sourceUrl()
+  ? await loadUrl(sourceUrl())
   : localStorage.getItem("source") ??
     await loadUrl("/examples/hailstone/hailstone.ptls");
 
@@ -46,7 +62,17 @@ const Console = {
   },
 };
 
-jar.onUpdate(saveSource);
+jar.onUpdate((code) => {
+  if (sourceUrl() && code !== source) {
+    window.history.pushState({}, "", window.location.pathname);
+  } else {
+    saveSource();
+  }
+});
+
+window.onpopstate = () => {
+  jar.updateCode(source);
+};
 
 const runtime = new ptls.Runtime({ ...ptls.impl, Console }, {});
 
@@ -62,7 +88,7 @@ run.onclick = async () => {
 
     await runtime.spawnEnv().eval(statements);
   } catch (err) {
-    output.innerText += err.toString();
+    output.innerText += await err.repr();
     throw err;
   }
 };
