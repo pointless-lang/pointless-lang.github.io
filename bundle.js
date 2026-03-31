c5a18e8d5b50a81c7e4572904455ad3420e52dcb
avery nortonsmith <averynortonsmith@fastmail.com>
2026-03-31 13:11:30 -0400

    simplify import paths

--- bundle.js
+++ bundle.js
@@ -1,30 +1,30 @@
 export { CodeJar } from "codejar";
 
-import { tokenize } from "./node_modules/pointless/lang/tokenizer.js";
-import { parse } from "./node_modules/pointless/lang/parser.js";
-import { Panic } from "./node_modules/pointless/lang/panic.js";
-import { highlight } from "./build/render/highlight.js";
-import { repr } from "./node_modules/pointless/lang/repr.js";
-import { Runtime } from "./node_modules/pointless/runtime/runtime.js";
+import { tokenize } from "pointless/lang/tokenizer.js";
+import { parse } from "pointless/lang/parser.js";
+import { Panic } from "pointless/lang/panic.js";
+import { Highlighter } from "pointless/utils/highlight.js";
+import { repr } from "pointless/lang/repr.js";
+import { Runtime } from "pointless/runtime/runtime.js";
 
 const impl = {
-  Async: await import("./node_modules/pointless/std/Async.js"),
-  Bool: await import("./node_modules/pointless/std/Bool.js"),
-  Char: await import("./node_modules/pointless/std/Char.js"),
-  // Console: await import("./node_modules/pointless/std/Console.js"),
-  Err: await import("./node_modules/pointless/std/Err.js"),
-  // Fs: await import("./node_modules/pointless/std/Fs.js"),
-  List: await import("./node_modules/pointless/std/List.js"),
-  Math: await import("./node_modules/pointless/std/Math.js"),
-  Obj: await import("./node_modules/pointless/std/Obj.js"),
-  Panic: await import("./node_modules/pointless/std/Panic.js"),
-  Rand: await import("./node_modules/pointless/std/Rand.js"),
-  Ref: await import("./node_modules/pointless/std/Ref.js"),
-  Re: await import("./node_modules/pointless/std/Re.js"),
-  Set: await import("./node_modules/pointless/std/Set.js"),
-  Str: await import("./node_modules/pointless/std/Str.js"),
-  Table: await import("./node_modules/pointless/std/Table.js"),
-  Test: await import("./node_modules/pointless/std/Test.js"),
+  Async: await import("pointless/std/Async.js"),
+  Bool: await import("pointless/std/Bool.js"),
+  Char: await import("pointless/std/Char.js"),
+  // Console: await import("pointless/std/Console.js"),
+  Err: await import("pointless/std/Err.js"),
+  // Fs: await import("pointless/std/Fs.js"),
+  List: await import("pointless/std/List.js"),
+  Math: await import("pointless/std/Math.js"),
+  Obj: await import("pointless/std/Obj.js"),
+  Panic: await import("pointless/std/Panic.js"),
+  Rand: await import("pointless/std/Rand.js"),
+  Ref: await import("pointless/std/Ref.js"),
+  Re: await import("pointless/std/Re.js"),
+  Set: await import("pointless/std/Set.js"),
+  Str: await import("pointless/std/Str.js"),
+  Table: await import("pointless/std/Table.js"),
+  Test: await import("pointless/std/Test.js"),
 };
 
 export const ptls = {
